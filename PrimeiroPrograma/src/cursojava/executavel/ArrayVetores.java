d66a1489d424ebd6827a326a63505a342c87cd07
Gustavo Lanjoni <gustavosilva94514@gmail.com>
2024-12-01 17:37:51 -0300

    Atualização

--- PrimeiroPrograma/src/cursojava/executavel/ArrayVetores.java
+++ PrimeiroPrograma/src/cursojava/executavel/ArrayVetores.java
@@ -1,26 +1,30 @@
 package cursojava.executavel;
 
+import javax.swing.JOptionPane;
+
 public class ArrayVetores {
 
 	public static void main(String[] args) {
 		
 		//Array pode ser todos tipos  de dados e objetos também
 		
+		
+		String posicoes = JOptionPane.showInputDialog("Quantas posições o Array deve ter?");
+		
+		
+		
+		
 		//Array sempre dever ter a quantdade de posições definida
-		double notas[] = new double[4];
+		double notas[] = new double[Integer.parseInt(posicoes)];
 		
-		//Atribuir valor nas posições do arrays 
-		notas[0] = 9.8;
-		notas[1] = 4.0;
-		notas[2] = 1.1;
-		notas[3] = 0.0;
+		for(int pos = 0; pos <notas.length; pos++) {
+			String valor = JOptionPane.showInputDialog("Qual o valor da posição " + pos);
+			notas[pos] = Double.valueOf(valor);
+		}
 		
-				
-		
-		System.out.println("nota 0 = " + notas[0]);
-		System.out.println("nota 1 = " + notas[1]);
-		System.out.println("nota 2 = " + notas[2]);
-		System.out.println("nota 3 = " + notas[3]);
+		for (int pos = 0; pos <notas.length; pos++) {
+			System.out.println("Nota " + (pos + 1) + " é " + notas[pos]);
+		}
 		
 	}
 	
