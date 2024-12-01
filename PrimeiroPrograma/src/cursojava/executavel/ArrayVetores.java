21d63ed3757ff09883bf2b9041e15b495d4fd48a
Gustavo Lanjoni <gustavosilva94514@gmail.com>
2024-12-01 17:59:14 -0300

    Atualiznado as notasd com Array

--- PrimeiroPrograma/src/cursojava/executavel/ArrayVetores.java
+++ PrimeiroPrograma/src/cursojava/executavel/ArrayVetores.java
@@ -6,12 +6,8 @@ public class ArrayVetores {
 
 	public static void main(String[] args) {
 
-		String valores[] = {"Gustavo", "19", "Cosmorama", "Sp"};
-		
-		
-		for (int pos = 0; pos <valores.length; pos++) {
-			System.out.println("Valor na posição " + (pos + 1) + " é " + valores[pos]);
-		}
+		int[] inteiros = new int[10];
+		float [] floats = new float[10];
 		
 		
 		
